10e9085307ec8e4a066b69cf34eab4d7550ce8e7
Gulzar Singh <gsingh29051982@gmail.com>
2023-08-24 11:46:35 -0400

    Create test_j.java
    I want to add to my Git

---
+++ test_j.java
@@ -0,0 +1,39 @@
+package test;
+
+/*Code for pattern
+
+      ******
+     ******
+    ******
+   ******
+  ******
+ ******
+ */
+
+public class test_j {
+	
+
+	public static void main(String[] args) {
+		int n=5;
+		int c=n;
+		for (int i=0; i<=n;i++){
+			for (int j=0; j<=n-i;j++){
+				
+				System.out.print(" ");}
+			
+			for(int j=0;j<=n;j++)
+				
+				{System.out.print("*");}
+			
+			
+			System.out.println();
+		}
+		
+				
+		}
+		
+	
+}
+
+
+
